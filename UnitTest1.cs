bb7dfe68916ddca4cd46889a4aa47c5f30b87c41
ganap <ganap@PGR>
2025-02-25 16:40:12 +0530

    Add project files.

---
+++ UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace TrumpMetamation_Task_Alarm
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
